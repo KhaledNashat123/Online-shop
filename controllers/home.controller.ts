a8022a41495faabce92bbbe83601d1d1fb3765fe
Khaled Nashat <KhaledNashat40@gmail.com>
2024-12-19 22:27:47 +0200

    fixing an error: give a default parameter to category

--- controllers/home.controller.ts
+++ controllers/home.controller.ts
@@ -5,7 +5,7 @@ import { getproductbycategory } from "../models/products.model";
 export const getHome = (req ,res ,next ) => {
 
     // to filter by the category
-    let category = req.query.category;
+    let category = req.query.category || "all";
     let allcatigories  : string[] = ["all" ,"silver" , "gold" ,"black"];
     
     if (allcatigories.includes(category) && category !== "all" ){
