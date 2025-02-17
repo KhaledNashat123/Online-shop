74eddeb99b827d82941920dbdb8173263e41aa6e
Khaled Nashat <KhaledNashat40@gmail.com>
2025-02-18 00:01:08 +0200

    checking if you are admin before going to manage orders page

--- routes/guards/admin.guard.ts
+++ routes/guards/admin.guard.ts
@@ -1,4 +1,4 @@
 export const isAdmin = (req,res,next) =>{
     if(req.session.isAdmin) return next()
-    return res.redirect('/')
+    return next("not admin");
 }
--- routes/order.route.ts
+++ routes/order.route.ts
@@ -2,15 +2,16 @@ import express from "express";
 import { getManageOrder, getOrders, updateOrderStatus } from "../controllers/order.controller";
 import { confirmOrders, deleteAllOrders } from "../controllers/order.controller";
 import { isauth } from "./guards/auth.guard";
+import { isAdmin } from "./guards/admin.guard";
 
 
 const router = express.Router();
 
 router.get("/", isauth, getOrders);
 
-router.get("/manage-orders", isauth, getManageOrder);
+router.get("/manage-orders", isauth,isAdmin, getManageOrder);
 
-router.post("/manage-orders", isauth, confirmOrders);
+router.post("/manage-orders", isauth,isAdmin, confirmOrders);
 
 router.post("/updateStatus/:orderId", isauth, updateOrderStatus);
 router.post("/deleteAll", isauth, deleteAllOrders);
